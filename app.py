7d708c194bfe72dccba2bfb53481ddcafbbc4e8e
tctg0arnav <thecrazytechgeek@gmail.com>
2023-03-19 15:10:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,31 +6,40 @@ import sqlite3
 app = Flask(__name__)
 app.secret_key = 'secretkey'
 
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-# db = SQLAlchemy(app)
+# users = {
+#     'student': ['student',1],
+#     'supervisor': ['supervisor',2],
+#     'doc_comm': ['doc_comm',3],
+#     'hoau': ['hoau',4],
+#     'adordc': ['adordc',5],
+#     'dr_a': ['dr_a',6]
+# }
 
-# class User(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(20), unique=True, nullable=False)
-#     password = db.Column(db.String(20), nullable=False)
-#     tier = db.Column(db.Integer, nullable=False)
-#     name = db.Column(db.String(20), nullable=False)
-#     surname = db.Column(db.String(20), nullable=False)
-#     email = db.Column(db.String(50), nullable=False)
-#     AU = db.Column(db.String(20), nullable=False)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
 
-# class project(db.Model):
-#     projectid = db.Column(db.Integer, primary_key=True)
-#     AU = db.Column(db.String(20), nullable=False)
-#     studentid = db.Column(db.Integer, nullable=False)
-#     date_of_registration = db.Column(db.date, nullable=False)
-#     GATE = db.Column(db.boolean, nullable=False)
-#     project_title = db.Column(db.String(50), nullable=False)
-#     date_of_irb = db.Column(db.date, nullable=False)
-#     date_of_progress_present = db.Column(db.date, nullable=False)
-#     file_path = db.Column(db.String(50), nullable=False)
-#     publications = db.Column(db.String(50), nullable=False)
-#     conferences = db.Column(db.String(50), nullable=False)
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    password = db.Column(db.String(20), nullable=False)
+    tier = db.Column(db.Integer, nullable=False)
+    name = db.Column(db.String(20), nullable=False)
+    surname = db.Column(db.String(20), nullable=False)
+    email = db.Column(db.String(50), nullable=False)
+    AU = db.Column(db.String(20), nullable=False)
+
+class project(db.Model):
+    projectid = db.Column(db.Integer, primary_key=True)
+    AU = db.Column(db.String(20), nullable=False)
+    studentid = db.Column(db.Integer, nullable=False)
+    date_of_registration = db.Column(db.date, nullable=False)
+    GATE = db.Column(db.boolean, nullable=False)
+    project_title = db.Column(db.String(50), nullable=False)
+    date_of_irb = db.Column(db.date, nullable=False)
+    date_of_progress_present = db.Column(db.date, nullable=False)
+    file_path = db.Column(db.String(50), nullable=False)
+    publications = db.Column(db.String(50), nullable=False)
+    conferences = db.Column(db.String(50), nullable=False)
     
 
 @app.route('/')
