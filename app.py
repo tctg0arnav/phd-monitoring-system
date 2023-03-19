2d642e90d81b933f9fcd026a53460c46c18b59b7
tctg0arnav <thecrazytechgeek@gmail.com>
2023-03-19 16:14:17 +0530

    .

--- app.py
+++ app.py
@@ -44,48 +44,9 @@ class project(db.Model):
 
 @app.route('/')
 def index():
-    # Redirect to login page
-    print(session)
-    return redirect(url_for('login'))
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    # Check if user is already logged in
-    if 'username' in session:
-        print(session)
-        return redirect(url_for('dashboard',tier=users[session['username']][1]))
-
-    if request.method == 'POST':
-        print(session)
-        username = request.form['username']
-        password = request.form['password']
-
-        # Check if username and password are valid
-        if username in users and password == users[username][0]:
-            session['username'] = username
-            print(session)
-            tier = users[username][1]
-            return redirect(url_for('dashboard', tier=tier))
-        else:
-            error = 'Invalid username or password.'
-            return render_template('login.html', error=error)
-    else:
-        return render_template('login.html')
-
-@app.route('/dashboard/<int:tier>')
-def dashboard(tier):
-    # Check if tier is valid
-    if 'username' not in session:
-        print(session)
-        return redirect(url_for('login'))
-    return render_template('dashboard.html', tier=tier)
+    return render_template('login.html')
 
 
-@app.route('/logout')
-def logout():
-    session.clear()
-    print(session)
-    return redirect(url_for('login'))
 
 if __name__ == '__main__':
     app.run(debug=True)
